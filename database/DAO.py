2c94021b20c3ce583ed8f901ca7b0048c1f6c6bf
samuelerestino <s299496@studenti.polito.it>
2024-07-03 19:50:27 +0200

    Versione finale ArtsMia

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,73 @@
 from database.DB_connect import DBConnect
+from model.Oggetto import Oggetto
+from model.Connessione import Connessione
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllObjects():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "select * from objects o"
+
+        cursor.execute(query, ())
+
+        for row in cursor:
+            result.append(Oggetto(**row))
+            # result.append(ArtObject(object_id=row["object_id"], ... ))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getPeso(self, v1: Oggetto, v2: Oggetto):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select count(*)
+            from exhibition_objects eo1, exhibition_objects eo2 
+            WHERE eo1.exhibition_id = eo2.exhibition_id 
+            and eo1.object_id < eo2.object_id 
+            and eo1.object_id = %s
+            and eo2.object_id = %s"""
+
+        cursor.execute(query, (v1.object_id, v2.object_id))
+
+        for row in cursor:
+            result.append(row)
+
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getAllConnessioni(idMap):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select eo1.object_id as o1, eo2.object_id as o2, count(*) as peso
+                    from exhibition_objects eo1, exhibition_objects eo2 
+                    WHERE eo1.exhibition_id = eo2.exhibition_id 
+                    and eo1.object_id < eo2.object_id 
+                    group by eo1.object_id, eo2.object_id
+                    order by peso desc"""
+
+        cursor.execute(query, ())
+
+        for row in cursor:
+            result.append(Connessione(idMap[row["o1"]],
+                                      idMap[row["o2"]],
+                                      row["peso"]
+                                      ))
+
+        cursor.close()
+        conn.close()
+        return result
